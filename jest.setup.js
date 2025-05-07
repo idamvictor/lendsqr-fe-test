36bd55c6ce38cc1b1f74777616ee7097f039d2e0
Victor Idam <idamvictorx1@gmail.com>
2025-05-07 13:47:36 +0100

    chore: add testing scripts and dependencies for Jest and Testing Library

---
+++ jest.setup.js
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+
+// Mock next/router
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    prefetch: jest.fn(),
+    pathname: '/',
+    query: {},
+  }),
+}));
